c1de028fd368b33e5faa7355d555e28a53b43e4b
David Sanchez <davidendubai@gmail.com>
2024-09-09 05:10:03 -0500

    Sping-boot and Java proyect init

---
+++ dream-shops/src/main/java/com/dailycodework1/dreamshops/model/Category.java
@@ -0,0 +1,27 @@
+package com.dailycodework1.dreamshops.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
